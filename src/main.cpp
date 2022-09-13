61988ffc94d79dfd3d2be49007afcbad5eb21e19
Max <57141754+m-camps@users.noreply.github.com>
2022-09-13 17:29:32 +0200

    launched branch

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,12 @@
 /*   By: mcamps <mcamps@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/13 16:55:08 by mcamps        #+#    #+#                 */
-/*   Updated: 2022/09/13 16:59:55 by mcamps        ########   odam.nl         */
+/*   Updated: 2022/09/13 17:28:46 by mcamps        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <sys/socket.h>
 
 int	main(int argc, char **argv)
 {
